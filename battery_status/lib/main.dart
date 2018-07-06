bde2e4aa3d3b98c6fa670047b9e87f72e9f7b996
Mohith7548 <mohith7548@gmail.com>
2018-07-06 13:29:40 +0530

    Battery Status -inital commit

---
+++ battery_status/lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Battery Status',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Battery Status'),
+      ),
+      body: Center(
+        child: Icon(
+          Icons.battery_std,
+          color: Colors.green,
+          size: 80.0,
+        ),
+      ),
+    );
+  }
+}
